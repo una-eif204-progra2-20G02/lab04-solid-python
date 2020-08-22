2dc751d52db6b44d54f4f26590dad00ca135c46d
fernandafallas <68918207+fernandafallas@users.noreply.github.com>
2020-08-21 21:40:27 -0600

    Update Physical.h
    commit

--- Physical.h
+++ Physical.h
@@ -5,10 +5,20 @@
 #ifndef LAB04_SOLID_PHYSICAL_H
 #define LAB04_SOLID_PHYSICAL_H
 
+#include "Game.h"
+#include "IReplace.h"
 
-class Physical {
+class Physical : IReplace, public Game {
+
+public:
+    Physical(string name, double price, double itemWeight, string productDimensions, double tax);
+
+    void replace() override;
+
+    string toString() override;
+
+    ~Physical();
 
 };
-
 
 #endif //LAB04_SOLID_PHYSICAL_H
