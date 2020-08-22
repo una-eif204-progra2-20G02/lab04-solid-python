9759fa5ee23962320c2e9ca4a90b51f72879dc08
fernandafallas <68918207+fernandafallas@users.noreply.github.com>
2020-08-21 21:38:35 -0600

    Update Digital.cpp
    commit

--- Digital.cpp
+++ Digital.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Digital.h"
+
+Digital::Digital(string name, double price, double itemWeight, string productDimensions, double tax) : Game(name, itemWeight, productDimensions,tax) {
+
+}
+
+void Digital::replace() { //este no seria un string ??
+
+}
+
+string Digital::toString() {
+    stringstream s;
+    s<<"Digital : \n"<<Game::toString()<< endl;
+    return s.str();
+}
+
+Digital::~Digital() {
+
+}
