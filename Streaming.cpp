1ce059856afae6fb650d60976b6860afb5c87192
fernandafallas <68918207+fernandafallas@users.noreply.github.com>
2020-08-21 21:35:26 -0600

    Update Streaming.cpp
    commit

--- Streaming.cpp
+++ Streaming.cpp
@@ -4,16 +4,20 @@
 
 #include "Streaming.h"
 
-void Streaming::replace() {
+Streaming::Streaming(string name, double price, double itemWeight, string productDimensions, double tax) : Game(name, itemWeight, productDimensions,tax) {
 
 }
 
-Streaming::Streaming() : Game() {
+void Streaming::replace() { //este no seria un string ?? 
     
 }
 
 string Streaming::toString() {
-
+    stringstream s;
+    s<<"Streaming : \n"<<Game::toString()<< endl;
+    return s.str();
 }
 
-Streaming::~Streaming() {}
+Streaming::~Streaming() {
+    
+}
