9cd84a5208e8ea9c0edf2f1df839ffd1baadaf02
fernandafallas <68918207+fernandafallas@users.noreply.github.com>
2020-08-21 21:41:00 -0600

    Update Physical.cpp
    commit

--- Physical.cpp
+++ Physical.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Physical.h"
+
+Physical::Physical(string name, double price, double itemWeight, string productDimensions, double tax) : Game(name, itemWeight, productDimensions,tax) {
+
+}
+
+void Physical::replace() { //este no seria un string ??
+
+}
+
+string Physical::toString() {
+    stringstream s;
+    s<<"Digital : \n"<<Game::toString()<< endl;
+    return s.str();
+}
+
+Physical::~Physical() {
+
+}
