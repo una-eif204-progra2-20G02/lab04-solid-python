26999a9a6622960854a2e7ea3c88eb5402d2d186
Fabi2094 <68891397+Fabi2094@users.noreply.github.com>
2020-08-24 10:24:00 -0600

    Update Streaming.cpp

--- Streaming.cpp
+++ Streaming.cpp
@@ -15,11 +15,15 @@ Streaming::Streaming(string name,double price,double tax):Game(name,price,tax){
 string Streaming::replace() {
     std::ostringstream output;
     output <<"this product will have a replacement" << endl;
+    return output.str();
 }
 
 string Streaming::toString() {
     std::ostringstream output;
     output << "Game Information: "<<Game::toString();
+    return output.str();
 }
 
-Streaming::~Streaming() {}
+Streaming::~Streaming() {
+
+}
